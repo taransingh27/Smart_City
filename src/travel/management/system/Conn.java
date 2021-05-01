b92b9c4f7ca72425129c0c31933cc7b0c6876aaa
TARAN SINGH <taran.singh_cs18@gla.ac.in>
2021-05-01 18:16:32 +0530

    Add files via upload

---
+++ src/travel/management/system/Conn.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package travel.management.system;
+
+import java.sql.*;  
+
+public class Conn{
+    Connection c;
+    Statement s;
+    public Conn(){  
+        try{  
+            Class.forName("com.mysql.cj.jdbc.Driver");  
+            
+            c =DriverManager.getConnection("jdbc:mysql://localhost:3306/mysql","root","12345678"); 
+            
+            s =c.createStatement();  
+            
+           
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+}  
